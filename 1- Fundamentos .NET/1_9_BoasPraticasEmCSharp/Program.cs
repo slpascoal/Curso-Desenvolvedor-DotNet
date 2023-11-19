25de29e06ba1f6f60112ac82aa03ea01a176dca3
Silas L. Pascoal <slpascoal01@gmail.com>
2023-11-19 17:29:16 -0300

    Aula Concluída: 9. Boas Práticas em C#

---
+++ 1- Fundamentos .NET/1_9_BoasPraticasEmCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using códigos.Models;
+
+Console.WriteLine("** Comentário de única linha **"); 
+// comentário
+Console.WriteLine("** Comentário de várias linhas **"); 
+/*
+    Comentário 1
+    Comentário 2
+*/
+Console.WriteLine("** Comentando nossas classes <summary> **");
+/// <summary>
+/// Váriavel de número inteiro
+/// </summary> 
+public class inteiros{
+    public int inteiro { get; set; }
+
+/// <summary>
+/// Método de soma
+/// </summary>
+/// <param name="x">O primeiro número da soma</param>
+/// <param name="y">O segundo número da soma</param>
+    public void Soma(int x, int y){
+        Console.WriteLine(x + y);
+    }
+}
